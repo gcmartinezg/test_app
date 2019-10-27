794fe3849d0a3fc357bcee1702500296f50f7a5e
German Martinez <germancmartinez@hotmail.com>
2019-10-26 23:23:23 -0500

    commit prueba

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/german/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/german/Documents/proyectos/test_firebase"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/german/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
